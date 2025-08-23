abec9921ee2ef72d4c0011b1c7b346ca68cb0e97
Shahnawaz <shahnawazhusain556@gmail.com>
2025-08-24 02:31:20 +0530

    data validation complete

---
+++ src/data_access/US_Company_Bankruptcy_data.py
@@ -0,0 +1,63 @@
+import sys
+import pandas as pd
+import numpy as np
+from typing import Optional
+
+from src.configuration.mongo_db_connection import MongoDBClient
+from src.constants import DATABASE_NAME
+from src.exception import MyException
+
+class US_Company_Bankruptcy_Data:
+    """
+    A class to export MongoDB records as a pandas DataFrame.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the MongoDB client connection.
+        """
+        try:
+            self.mongo_client = MongoDBClient(database_name=DATABASE_NAME)
+            print("Collections in DB:", self.mongo_client.database.list_collection_names())
+            
+        except Exception as e:
+            raise MyException(e, sys)
+
+    def export_collection_as_dataframe(self, collection_name: str, database_name: Optional[str] = None) -> pd.DataFrame:
+        """
+        Exports an entire MongoDB collection as a pandas DataFrame.
+
+        Parameters:
+        ----------
+        collection_name : str
+            The name of the MongoDB collection to export.
+        database_name : Optional[str]
+            Name of the database (optional). Defaults to DATABASE_NAME.
+
+        Returns:
+        -------
+        pd.DataFrame
+            DataFrame containing the collection data, with '_id' column removed and 'na' values replaced with NaN.
+        """
+        try:
+            # Access specified collection from the default or specified database
+            if database_name is None:
+                collection = self.mongo_client.database[collection_name]
+                count = self.mongo_client.database[collection_name].count_documents({})
+                print(f"Documents in {collection_name}: {count}")
+                doc = self.mongo_client.database[collection_name].find_one()
+                print(doc)
+            else:
+                collection = self.mongo_client[database_name][collection_name]
+
+            # Convert collection data to DataFrame and preprocess
+            print("Fetching data from mongoDB")
+            df = pd.DataFrame(list(collection.find()))
+            print(f"Data fecthed with len: {len(df)}")
+            if "id" in df.columns.to_list():
+                df = df.drop(columns=["id"], axis=1)
+            df.replace({"na":np.nan},inplace=True)
+            return df
+
+        except Exception as e:
+            raise MyException(e, sys)
