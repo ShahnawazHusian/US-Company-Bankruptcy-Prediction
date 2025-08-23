ad653bceaed93a217dbab69343ebc5c36c918303
Shahnawaz <shahnawazhusain556@gmail.com>
2025-08-23 19:58:52 +0530

    logging and exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shahnawaz Husain",
+    author_email="husainshahnawaz15@gmail.com",
+    packages=find_packages(where="src")
+)
