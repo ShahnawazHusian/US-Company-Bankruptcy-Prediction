c8528412649c2a4607f7e31b335d646df5761b55
Shahnawaz <shahnawazhusain556@gmail.com>
2025-09-05 00:25:52 +0530

    workflow setup complete

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
-from src.pipline.training_pipeline import TrainPipeline
+# from src.pipline.training_pipeline import TrainPipeline
 
-pipeline = TrainPipeline()
-pipeline.run_pipeline()
+# pipeline = TrainPipeline()
+# pipeline.run_pipeline()
+
+import os
+from src.constants import MONGODB_URL_KEY
+
+mongo_url = os.getenv(MONGODB_URL_KEY)
+if not mongo_url:
+    raise Exception(f"Environment variable '{MONGODB_URL_KEY}' is not set.")
+else:
+    print("connected")
